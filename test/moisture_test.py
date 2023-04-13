ac250c26f9ba995f7800ab754e352defad30fb2b
Jonas Jansen <git@jonasjansen.de>
2023-04-13 08:19:05 +0200

    Add hardware test scripts.

---
+++ test/moisture_test.py
@@ -0,0 +1,15 @@
+from machine import ADC, Pin
+import utime
+
+soil = ADC(Pin(26))
+
+min_moisture=0
+max_moisture=65535
+
+readDelay = 0.5
+
+while True:
+    moisture = (max_moisture-soil.read_u16())*100/(max_moisture-min_moisture)
+    print("moisture: " + "%.2f" % moisture +"% (adc: "+str(soil.read_u16())+")")
+    utime.sleep(readDelay)
+
