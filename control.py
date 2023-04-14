06972c3cbf90f31d37ae65044aab556a327441a5
Jonas Jansen <git@jonasjansen.de>
2023-04-14 16:40:42 +0200

    Add empty logger and control files.

---
+++ control.py
@@ -0,0 +1,8 @@
+# Actual control
+
+
+def run():
+    # get sensor values
+    # log sensor values
+    # control devices.
+    pass
---
+++ logger.py
@@ -0,0 +1,7 @@
+# Logger - Entry point for logging information to file and into database.
+
+class Logger:
+    pass
+
+
+logger = Logger()
