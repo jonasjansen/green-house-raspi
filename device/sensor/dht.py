3a4a35312d5c24cdeb6336daaa03f3958e694355
Jonas Jansen <git@jonasjansen.de>
2023-04-15 22:38:41 +0200

    Update

--- device/sensor/dht.py
+++ device/sensor/dht.py
@@ -1,11 +1,12 @@
 import statistics
-from logger import logger
+from time import sleep
+
 import Adafruit_DHT
+import adafruit_dht
 
 from config_provider import config
+from logger import logger
 
-import adafruit_dht
-import board
 
 class DHT:
 
@@ -13,20 +14,28 @@ class DHT:
         self.gpio = config.get_config('GPIO/DHT/DATA_PIN')
         self.humidity = 0
         self.temperature = 0
-        self.dhtDevice = adafruit_dht.DHT11(board.D4)
+        self.dhtDevice = adafruit_dht.DHT11(self.gpio)
 
     def read_values(self):
         try:
             humidity_vals = []
             temperature_vals = []
             # get 10 values and calculate median. Lowers the chances of wrong data.
-            for i in range(10):
+            for i in range(3):
                 try:
-                    humidity_vals.append(self.dhtDevice.humidity)
-                    temperature_vals.append(self.dhtDevice.temperature)
+                    humidity, temperature = Adafruit_DHT.read_retry(11, self.gpio)
+
+                    # the new method returns "None" very often. Therefore, it is not used here.
+                    # humidity = self.dhtDevice.humidity
+                    # temperature = self.dhtDevice.temperature
+                    humidity_vals.append(humidity)
+                    temperature_vals.append(temperature)
+
+                    sleep(0.1)
                 except RuntimeError as error:
                     # Errors happen fairly often, DHT's are hard to read, just keep going
                     logger.debug(str(error))
+                    sleep(0.1)
                     continue
 
             self.temperature = statistics.median(temperature_vals)
@@ -35,7 +44,6 @@ class DHT:
         except Exception as error:
             self.dhtDevice.exit()
             logger.error(str(error))
-
 
     def get_temperature(self):
         self.read_values()
--- device/sensor/moisture.py
+++ device/sensor/moisture.py
@@ -1,8 +1,13 @@
+import statistics
+from time import sleep
+
+import adafruit_ads1x15.ads1115 as ADS
+import board
+import busio
+from adafruit_ads1x15.analog_in import AnalogIn
+
 from config_provider import config
 
-
-# install via 'sudo pip install Adafruit-DHT'
-# NOTE: Is deprecated. Might want to replace it with https://pypi.org/project/adafruit-circuitpython-dht/
 
 class Moisture:
 
@@ -10,9 +15,26 @@ class Moisture:
         self.gpio = config.get_config('GPIO/MOISTURE/DATA_PIN')
         self.moisture = 0
 
+        i2c = busio.I2C(board.SCL, board.SDA)
+        # Create the ADC object using the I2C bus
+        ads = ADS.ADS1115(i2c)
+        # Create single-ended input on channels
+        self.chan0 = AnalogIn(ads, ADS.P0)
+        self.chan1 = AnalogIn(ads, ADS.P1)
+        self.chan2 = AnalogIn(ads, ADS.P2)
+        self.chan3 = AnalogIn(ads, ADS.P3)
+
     def get_moisture(self):
-        # TODO: Return correct value
-        return 60
+        self.read_moisture()
+        return self.moisture
+
+    def read_moisture(self):
+        values = []
+        for i in range(10):
+            values.append(self.chan0.voltage)
+            sleep(0.1)
+        print(values)
+        self.moisture = "{:>5.3f}".format(statistics.median(values))
 
 
 moisture = Moisture()
